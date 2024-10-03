9d0df61c7fd0989fa81b8d4a5acb6b0b8ef20176
Ali-maoie <git21416@gmail.com>
2024-10-03 15:51:07 +0100

    Add files via upload

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,16 +1,17 @@
-/*import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 import { levelup } from '../lib/canvas.js'
+
 export function before(m, { conn }) {
 
-let user = global.db.data.users[m.sender]
-let chat = global.db.data.chats[m.chat]
-if (!chat.autolevelup)
-return !0
+    let user = global.db.data.users[m.sender]
+    let chat = global.db.data.chats[m.chat]
+    if (!chat.autolevelup)
+        return !0
 
-let before = user.level * 1
-while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
- if (before !== user.level) {
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 
-conn.reply(m.chat, `🚩 *SUBISTE DE NIVEL*\n\n🍟 *Nuevo Nivel:* ${user.level}\n✨️ *Nivel Anterior:* ${before}\n🍭 *Rango:* ${user.role}\n🗓 *Fecha:* ${new Date().toLocaleString('id-ID')}`.trim(),m, rcanal, )
+    if (before !== user.level) {
+        conn.reply(m.chat, `🚩 *لقد ارتقيت إلى مستوى جديد*\n\n🍟 *المستوى الجديد:* ${user.level}\n✨️ *المستوى السابق:* ${before}\n🍭 *الرتبة:* ${user.role}\n🗓 *التاريخ:* ${new Date().toLocaleString('ar-EG')}`.trim(), m, rcanal)
     }
-}*/
+}
